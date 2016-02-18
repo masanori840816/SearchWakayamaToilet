d86ef7c6156018859a5c208e3a436edaf1279d02
masanori_msl <masanori840816@gmail.com>
2016-02-18 22:34:17 +0900

    added Mockito

--- app/src/androidTest/java/jp/searchwakayamatoilet/MainActivityTest.java
+++ app/src/androidTest/java/jp/searchwakayamatoilet/MainActivityTest.java
@@ -9,6 +9,7 @@ import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.SearchView;
 import android.test.suitebuilder.annotation.LargeTest;
 import android.view.KeyEvent;
 import android.widget.EditText;
@@ -17,44 +18,56 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.spy;
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MainActivityTest {
+
     @Rule
     public ActivityTestRule<MainActivity> mainActivityRule = new ActivityTestRule(MainActivity.class);
 
     @Before
     public void setUp() throws Exception {
+    }
+    /*    @Test
+        public void hasToolbar() throws Exception{
+            onView(ViewMatchers.withId(R.id.toolbar)).check(matches(isDisplayed()));
+        }
+        @Test
+        public void hasSearchView() throws Exception{
+            onView(ViewMatchers.withId(R.id.searchview)).check(matches(isDisplayed()));
+        }
+        @Test
+        public void hasEditTextOnSearchView() throws Exception{
+            onView(ViewMatchers.withId(R.id.searchview)).perform(ViewActions.click());
+            onView(ViewMatchers.isAssignableFrom(EditText.class)).perform(ViewActions.typeText("test"), ViewActions.pressKey(66));
+        }
+        @Test
+        public void isSuggestSearchAllShown() throws Exception{
+            onView(ViewMatchers.withId(R.id.searchview)).perform(ViewActions.click());
+            onView(ViewMatchers.isAssignableFrom(EditText.class)).perform(ViewActions.typeText(" "), ViewActions.replaceText(""));
+            onView(ViewMatchers.withId(R.id.suggest_list)).check(matches(isDisplayed()));
+        }*/
+    @Test
+    public void isMethodSetMarkersByFreeWordCalled() throws Exception {
+        MainActivity activity = mock(MainActivity.class);
+        MainPresenter presenter = mock(MainPresenter.class);
 
-    }
-    @Test
-    public void hasToolbar() throws Exception{
-        onView(ViewMatchers.withId(R.id.toolbar)).check(matches(isDisplayed()));
-    }
-    @Test
-    public void hasSearchView() throws Exception{
-        onView(ViewMatchers.withId(R.id.searchview)).check(matches(isDisplayed()));
-    }
-    @Test
-    public void hasEditTextOnSearchView() throws Exception{
         onView(ViewMatchers.withId(R.id.searchview)).perform(ViewActions.click());
-        onView(ViewMatchers.isAssignableFrom(EditText.class)).perform(ViewActions.typeText("test"), ViewActions.pressKey(66));
-    }
-    @Test
-    public void isSuggestSearchAllShown() throws Exception{
-        onView(ViewMatchers.withId(R.id.searchview)).perform(ViewActions.click());
-        onView(ViewMatchers.isAssignableFrom(EditText.class)).perform(ViewActions.typeText(" "), ViewActions.replaceText(""));
-        onView(ViewMatchers.withId(R.id.suggest_list)).check(matches(isDisplayed()));
-    }
-    @Test
-    public void isMethodSetMarkersByFreeWordCalled() throws Exception{
-        hasEditTextOnSearchView();
+        onView(ViewMatchers.isAssignableFrom(EditText.class)).perform(ViewActions.typeText("test"), ViewActions.pressKey(KeyEvent.KEYCODE_ENTER));
+        verify(presenter).setMarkersByFreeWord("");
 
     }
 }
